660476ea3212e5d2b98cb69271382ada022d09ba
dichha <dichha.chamling@gmail.com>
2020-01-29 23:33:46 -0800

    added values from environment

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret.txt') as f:
-    SECRET_KEY = f.readline().strip()
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '')
 
 ALLOWED_HOSTS = []
 
