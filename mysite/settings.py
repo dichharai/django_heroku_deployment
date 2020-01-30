44a2e1e31a18fbb5a9c8f88f882d0d8b7ff2c369
dichha <dichha.chamling@gmail.com>
2020-01-29 23:38:58 -0800

    added app to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['morning-sea-62467.herokuapp.com']
 
 
 # Application definition
