0f244e924d64ab21a0246c3a6c31ddb0fb839ebf
dichha <dichha.chamling@gmail.com>
2020-01-30 21:22:43 -0800

    added localhose to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '')
 
-ALLOWED_HOSTS = ['morning-sea-62467.herokuapp.com']
+ALLOWED_HOSTS = ['morning-sea-62467.herokuapp.com', 'localhost']
 
 
 # Application definition
